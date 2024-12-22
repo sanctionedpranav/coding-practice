4d28b152925b06f39ee292e82a220193b9da4c8b
sanctionedpranav <pranav9728@gmail.com>
2024-12-22 21:21:30 +0530

    6 basic patterns complete

---
+++ InvertedHalfPyramid180.java
@@ -0,0 +1,19 @@
+
+public class InvertedHalfPyramid180 {
+
+    public static void main(String[] args) {
+        int n = 4;
+        //outer loop 
+        for (int i = 1; i <= n; i++) {
+            //inner loop for space print
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            //inner loop for * print
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
