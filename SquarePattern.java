4a5be1ddd169d8434254b28a3ea7ea3b635a9865
sanctionedpranav <pranav9728@gmail.com>
2024-12-22 12:57:20 +0530

    update pattern

--- SquarePattern.java
+++ SquarePattern.java
@@ -1,14 +1,14 @@
 
 public class SquarePattern {
   public static void main(String args[]) {
-      int n = 4; // Number of rows
-      int m = 5; // Number of columns
+      int n = 4;
+      int m = 5;
 
       for (int i = 1; i <= n; i++) {
           for (int j = 1; j <= m; j++) {
               System.out.print("*");
           }
-          System.out.println(); // Move to the next line after each row
+          System.out.println();
       }
   }
 }
