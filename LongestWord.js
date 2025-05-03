35532e7a90f4116386edae5e3157e892bac9e16a
sanctionedpranav <pranav9728@gmail.com>
2025-05-03 22:24:39 +0530

    find the longest word function.

---
+++ LongestWord.js
@@ -0,0 +1,15 @@
+function longestWord(sentence) {
+  const words = sentence.split(' ');
+  let longestWord = '';
+
+  for (let word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+
+  return longestWord;
+
+}
+
+console.log(longestWord("I love coding in Javascript"));
